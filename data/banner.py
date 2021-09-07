0d6d769a2e1d7f0ee3cefea1063297316de57f0a
Lursy <70035797+Lursy@users.noreply.github.com>
2021-09-07 11:08:17 -0300

    Update banner.py

--- data/banner.py
+++ data/banner.py
@@ -1,5 +1,5 @@
-from data.ferramentas import *
-from data.menu import bannerf, menu_cores, ban_sen, styles
+from Persux.data.ferramentas import *
+from Persux.data.menu import *
 
 
 def banner():
@@ -14,7 +14,7 @@ def banner():
             print(ban_sen)
             ban_exists = str(input(f'{am}//: {br}'))
             if ban_exists == '1':
-                os.system('clear')
+                clear()
                 pass
             elif ban_exists == '2':
                 os.system('rm -rf .Nick')
@@ -27,7 +27,7 @@ def banner():
                 sleep(1)
                 break
             elif ban_exists == '3':
-                os.system('clear')
+                clear()
                 break
             else:
                 print(f'{ve}{ban_exists} não é um comando')
@@ -35,15 +35,14 @@ def banner():
                 break
         os.system('clear')
         print(bannerf)
-        name = nick = str(input(f'{am}Nick: {br}'))
-        os.system('clear')
+        nick = str(input(f'{am}Nick: {br}'))
+        clear()
         print(f"{rx}{figlet.renderText('Styles')}")
         print(styles)
-        int_styles = str(input(f'{am}//: {br}'))
+        print(f'{vd}[i] {br}formatação do banner\n')
+        int_styles = str(input(f'\n{am}//: {br}'))
         if int_styles == '1':
-            banner_nick = f'''echo -e '\e[0;:corm'
-figlet {nick}
-echo -e '\e[m\\n' '''
+            name = figlet.renderText(f'{nick}')
         elif int_styles == '2':
             name = poison.renderText(f'{nick}')
         elif int_styles == '3':
@@ -52,51 +51,63 @@ echo -e '\e[m\\n' '''
             name = block.renderText(f'{nick}')
         elif int_styles == '5':
             name = alligator.renderText(f'{nick}')
+        elif int_styles == '6':
+            name = cosmic.renderText(f'{nick}')
+        elif int_styles == '7':
+            name = rozzo.renderText(f'{nick}')
         else:
             print(f'{ve}Comando não identificado!')
             sleep(2)
             break
-        if int_styles != '1':
-            banner_nick = f'''echo -e '\e[0;:corm'
-echo """{name}"""
-echo -e '\e[m\\n' '''
-        os.system('clear')
-        nick_banner = banner_nick.replace(':name', f'{nick}')
-        os.system('clear')
+        clear()
+        sleep(1)
+        print(f'{vd}\nbanner selecionado\n\n{br}{name}')
+        sleep(2)
+        banner = open('banner', 'w')
+        banner.write(f'{name}'.rstrip())
+        banner.close()
+        clear()
+        cat_banner = '''echo -e '\e[0;:corm'
+cat /data/data/com.termux/files/usr/etc/banner
+echo -e '\e[m\\n\\n' '''
+        clear()
         print(f"{az}{figlet.renderText('Cores')}")
         print(menu_cores)
+        print(f'{vd}[i] {br}cor do banner\n')
         cor_name = str(input(f'\n{am}//: {br}'))
         os.chdir('/data/data/com.termux/files/usr/etc')
         os.system('rm -rf bash.bashrc')
         if cor_name == '1':
-            snick = nick_banner.replace(':cor', '31')
+            snick = cat_banner.replace(':cor', '31')
         elif cor_name == '2':
-            snick = nick_banner.replace(':cor', '33')
+            snick = cat_banner.replace(':cor', '33')
         elif cor_name == '3':
-            snick = nick_banner.replace(':cor', '37')
+            snick = cat_banner.replace(':cor', '37')
         elif cor_name == '4':
-            snick = nick_banner.replace(':cor', '32')
+            snick = cat_banner.replace(':cor', '32')
         elif cor_name == '5':
-            snick = nick_banner.replace(':cor', '36')
+            snick = cat_banner.replace(':cor', '36')
         elif cor_name == '6':
-            snick = nick_banner.replace(':cor', '34')
+            snick = cat_banner.replace(':cor', '34')
         elif cor_name == '7':
-            snick = nick_banner.replace(':cor', '35')
+            snick = cat_banner.replace(':cor', '35')
         elif cor_name.lower() == 'lursy':
-            snick = f'echo """{name}"""|lolcat' if int_styles != '1' else f'figlet {nick}|lolcat'
+            snick = f"cat /data/data/com.termux/files/usr/etc/banner|lolcat && echo -e '\\n\\n'"
         elif cor_name == '8':
             break
         else:
-            print(f'{ve}{snick} não é um comando')
+            print(f'{ve}{cor_name} não é um comando')
             sleep(2)
-            os.system('clear')
+            clear()
             break
         nick_file = open('.Nick', 'w')
         nick_file.write(f'{snick}')
         nick_file.close()
         rnick = open('.Nick', 'r')
         bash = open('bash.bashrc', 'w')
-        bash.write(f'{inicio}\n' + f'{rsenha.read()}\n{rnick.read()}\n{rcor.read()}'if vs
-                   else f'{rnick.read()}\n{rcor.read()}\n{final}')
+        bash.write(f'{inicio}\n{rsenha.read()}\n{rnick.read()}\n{rcor.read()}\n{final}'if vs
+                   else f'{inicio}\n{rnick.read()}\n{rcor.read()}\n{final}')
         bash.close()
+        print(f'{vd}!Sucess!')
+        sleep(1)
         break
