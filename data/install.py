375a827f88c45f4c5ec6e36a3242a185bb86ae7f
Lursy <70035797+Lursy@users.noreply.github.com>
2021-09-07 11:12:24 -0300

    Update install.py

--- data/install.py
+++ data/install.py
@@ -1,36 +1,30 @@
 import os
+from Persux.data.cores_persux import *
 
 
 def install():
-    global nms
     os.chdir('/data/data/com.termux/files/usr/etc/')
-    Ver = os.path.exists('.usuario')
-    if not Ver:
+    inst = os.path.exists('.usuario')
+    if not inst:
         os.system('clear')
-        user = str(input(f'\033[0;32mNome: \033[0;31m'))
+        user = str(input(f'{vd}Nome: {ve}'))
         os.system('clear')
-        print(f'\033[0;32minstalando.')
+        print(f'{vd}instalando.')
+        os.system('python3 -m pip install --upgrade pip &>/dev/null')
         os.system('pkg install figlet -y &>/dev/null')
         os.system('clear')
-        print(f'\033[0;32minstalando..')
+        print(f'{vd}instalando..')
         os.system('pip install lolcat &>/dev/null')
         os.system('clear')
-        print(f'\033[0;32minstalando...')
-        try:
-            from pyfiglet import Figlet
-        except ModuleNotFoundError:
-            os.system('python3 -m pip install --upgrade pip &>/dev/null')
-            os.system('pip install pyfiglet &>/dev/null')
+        print(f'{vd}instalando...')
+        os.system('pip install pyfiglet &>/dev/null')
         os.system('clear')
-        os.chdir('/data/data/com.termux/files/usr/etc/')
         os.system('rm -rf .Cor')
         cor = open('.Cor', 'w')
         cor.write(f"PS1='\[\e[0;32m\]\w\[\e[0m\] \[\e[0;97m\]\$\[\e[0m\] '")
         cor.close()
         os.system('rm -rf motd')
-        os.chdir('/data/data/com.termux/files/usr/etc')
         user_file = open('.usuario', 'w')
         user_file.write(user)
         user_file.close()
         os.system('clear')
-        nms = 1
