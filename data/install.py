4d101b07bf93db5b5205651bf32ba1e374171243
Lursy <70035797+Lursy@users.noreply.github.com>
2021-09-07 11:55:35 -0300

    Update install.py

--- data/install.py
+++ data/install.py
@@ -3,9 +3,6 @@ from data.cores_persux import *
 
 
 def install():
-    os.system('clear')
-    print(f'{vd}verificando atualizações')
-    os.system('git pull &>/dev/null')
     os.chdir('/data/data/com.termux/files/usr/etc/')
     inst = os.path.exists('.usuario')
     if not inst:
